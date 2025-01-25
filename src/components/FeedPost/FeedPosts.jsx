0017e9bb2f26a92c97aafcb6b118fb79c3db6949
Aswani-K8 <wwwaswanik2003@gmail.com>
2025-01-25 22:36:56 +0530

    my commits

--- src/components/FeedPost/FeedPosts.jsx
+++ src/components/FeedPost/FeedPosts.jsx
@@ -1,28 +1,53 @@
-import { Container, VStack ,Skeleton,SkeletonCircle,Flex,Box} from '@chakra-ui/react'
-import FeedPost from './FeedPost';
-import useGetFeedPosts from "../../hooks/useGetFeedPosts";
+import { Box, Container, Flex, Text } from "@chakra-ui/react";
+import FeedPosts from "../../components/FeedPost/FeedPosts";
+import SuggestedUsers from "../../components/SuggestedUsers/SuggestedUsers";
+import useAuthStore from "../../store/authStore";
 
-const FeedPosts = () => {
-  const { isLoading, posts } = useGetFeedPosts();
+const HomePage = () => {
+  const authUser = useAuthStore((state) => state.user); // Get the authenticated user info
 
   return (
-    <Container maxW={"container.sm"} py={10} px={2} >
-      {isLoading && [0,1,2].map((_,idx) => (
-        <VStack key={idx} gap={4} alignItems={'flex-start'} mp={10}>
-          <Flex gap={2}>
-            <SkeletonCircle size='10' />
-            <VStack gap={"2"} alignItems={"flex-start"}>
-              <Skeleton height='10px' w={'200px'}/>
-              <Skeleton height='10px' w={'200px'}/>
-            </VStack>
-          </Flex>
-          <Skeleton w="full">
-            <Box h={'400px'}>Contents wrapped</Box>
-          </Skeleton>
-        </VStack>
-      ))}
-    {!isLoading && posts.map((post) => <FeedPost key={post.id} post={post} />)}
+    <Container maxW={"container.lg"} fontFamily="Sora, sans-serif">
+      <Flex direction="column" gap={10} py={10}>
+        {/* Welcome Message */}
+        <Box bg="teal.100" p={4} rounded="md" textAlign="center">
+          <Text fontSize="xl" fontWeight="bold">
+            Welcome {authUser.userType === "Innovator" ? "Back, Innovator" : "to the Portal"}!
+          </Text>
+        </Box>
+
+        {/* Conditional Layout Based on userType */}
+        <Flex gap={10}>
+          {authUser.userType === "Innovator" ? (
+            <>
+              {/* Content for Innovators */}
+              <Box flex={2} py={10} top={0}>
+                <FeedPosts />
+              </Box>
+              <Box
+                flex={3}
+                mr={50}
+                display={{ base: "none", lg: "block" }}
+                maxW={"300px"}
+                borderLeft={"1px solid #127B7E"}
+              >
+                <SuggestedUsers />
+              </Box>
+            </>
+          ) : (
+            <>
+              {/* Content for Other Users */}
+              <Box flex={1} py={10} textAlign="center">
+                <Text fontSize="lg" color="gray.600">
+                  Welcome to the portal! Explore and create events to connect with others.
+                </Text>
+              </Box>
+            </>
+          )}
+        </Flex>
+      </Flex>
     </Container>
-  )
-}
-export default FeedPosts;
+  );
+};
+
+export default HomePage;
